25f0377d7b8dcb51887f82741148b763744eac4e
Eric Anderson <ejander@gmail.com>
2018-11-06 22:36:49 -0600

    GuessNumber Game translated from Python to Java

---
+++ src/GuessNumber.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+import java.util.Random;
+
+
+public class GuessNumber {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        Random ran = new Random();
+
+        int guessesTaken = 0;
+        int guess = 999;
+        int target = ran.nextInt(20) + 1;
+        System.out.println("target = " + target);
+
+        while (guessesTaken < 6) {
+            guessesTaken = guessesTaken + 1;
+            System.out.println("Take a guess.");
+            guess =  reader.nextInt();
+
+            if (guess < target) {
+                System.out.println(guess + " is too low");
+            } else
+            if (guess > target) {
+                System.out.println(guess + " is too high");
+            } else {
+                System.out.println(guess + " is correct!!");
+                System.out.println("You got it in " + guessesTaken + " guesses.");
+                System.exit(0);
+            }
+        }
+        System.out.println("You have had  " + guessesTaken + "guesses.");
+        System.out.println("The number was:  " + target);
+
+        System.exit( 1);
+
+    }
+}
